715668d65ec7b1cc2e0eed06ae3304fdc8763203
mccarthyr <mccarthy.richard@gmail.com>
2017-12-21 19:32:34 +0000

    Initial import of SpringRestAPI project files

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringRestAPI - Learning Project
---
+++ src/main/java/com/fireduptech/spring/rest/security/MyRestAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.fireduptech.spring.rest.security;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+/**
+ * The Entry Point will not redirect to any sort of Login
+ * Instead it will return a Http Status 401
+ */
+
+public final class MyRestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence( final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException ) throws IOException {
+
+		response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized" );
+	}
+
+}
